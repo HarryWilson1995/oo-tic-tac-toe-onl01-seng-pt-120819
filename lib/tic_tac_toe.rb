1c6b264925e0306701df8e1171f652d4fe3f3f26
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-09 17:54:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,10 +55,10 @@ WIN_COMBINATIONS = [
   end 
   
   def turn 
-    puts "Player #{current_player}, please input a number 1-9: "
+    cp = current_player
+    puts "Player #{cp}, please input a number 1-9: "
     input = gets.strip 
     index = input_to_index(input)
-    cp = current_player
     if valid_move?(index)
       move(index, cp)
       display_board
